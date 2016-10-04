fd07faa5824781115177e048306c44b83b7890bf
Ismael Luque Jiménez <isluji@gmail.com>
2016-10-04 19:24:27 +0200

    Ejercicios del tema 1

---
+++ tema1/ej3/ej3.py
@@ -0,0 +1,8 @@
+print "Este programa muestra el producto de dos numeros.\n"
+
+a = input("Ingrese el primer factor: ")
+b = input("Ingrese el segundo factor: ")
+
+producto = a * b
+
+print "\nEl resultado del producto es:", producto
